d2a1fa7b507cd3f65e70e6c10afaeed25a485541
Suraj <surajworkspace12@gmail.com>
2025-08-26 13:46:33 +0545

    first ui check

---
+++ doko/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import './App.css'
+
+function App() {
+  return (
+    <div className="font-sans">
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
